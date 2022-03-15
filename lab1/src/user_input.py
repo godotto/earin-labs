566ab8cd97c6d69ecebd46188041f03a33f5058b
godotto <m.marcinkiewicz@protonmail.com>
2022-03-15 01:00:52 +0100

    Coefficients input for F(x)

--- lab1/src/user_input.py
+++ lab1/src/user_input.py
@@ -11,3 +11,18 @@ def integer_input(prompt):
         return int(input(prompt))
     except ValueError:
         return False
+
+def float_list_input(prompt):
+    """
+    Takes input from stdin and checks if
+    it is a proper list of floating-point
+    numbers. Returns an empty list if it
+    is not.
+
+    Parameters:
+    prompt: prompt message for user input.
+    """
+    try:
+        return [float(x) for x in input(prompt).split()]
+    except ValueError:
+        return []
--- lab1/src/user_interface.py
+++ lab1/src/user_interface.py
@@ -65,8 +65,26 @@ def select_objective_function():
     return (of.f_x, of.f_x_gradient) if function == 1 else (of.g_x, of.g_x_gradient)
 
 def select_parameters(objective_function):
-    pass
+    clear_console()
+
+    if objective_function == of.f_x:
+        coefficients_list = []
+        while len(coefficients_list) != 4:
+            print("Provide four scalar coefficients (a, b, c, d) for F(x) = ax^3 + bx^2 + cx + d")
+            coefficients_list = uin.float_list_input("<separate them with space>: ")
+
+            if len(coefficients_list) == 0:
+                print("At least one coefficient is wrong")
+                sleep(1)
+                clear_console()
+            elif len(coefficients_list) != 4:
+                print("Wrong number of coefficients")
+                sleep(1)
+                clear_console()
 
 def user_interface():
     optimization_method = select_optimization_method()
     objective_function, gradient = select_objective_function()
+    select_parameters(objective_function)
+
+user_interface()
