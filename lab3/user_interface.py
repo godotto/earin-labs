c6b3ba876da6af980ef4515290830cb015ea9ff7
C-url <kasia.bielecka999@gmail.com>
2022-04-11 11:38:45 +0200

    correct UI, add if user starts game choice

--- lab3/user_interface.py
+++ lab3/user_interface.py
@@ -20,40 +20,57 @@ def clear_console():
 
     os.system(clear_command)
 
+def is_user_first():
+    clear_console()
+
+    user_choice = ''
+    while (user_choice !='y' and user_choice !='n') or not user_choice:
+        print("Pick if you want to play first: 'y' if yes or 'n' if no")
+        user_choice = uin.char_input()
+
+        if  (user_choice !='y' and user_choice !='n') or not user_choice:
+            print("You have to type either 'y' or 'n'")
+            sleep(1)
+            clear_console()
+   
+    if user_choice=='y': 
+       return True
+    else:
+        return False    
 
 def get_players():
  
     clear_console()
 
-    user_player = ''
-    while (user_player !='o' and user_player !='x') or not user_player:
+    user_choice = ''
+    while (user_choice !='o' and user_choice !='x') or not user_choice:
         print("Pick which do you want to play with: x or o")
-        user_player = uin.char_input()
+        user_choice = uin.char_input()
 
-        if  (user_player !='o' and user_player !='x') or not user_player:
+        if  (user_choice !='o' and user_choice !='x') or not user_choice:
             print("You have to type either 'x' or 'o'")
             sleep(1)
             clear_console()
     algorithm_player = ''
-    if user_player=='o': 
+    if user_choice=='o': 
        algorithm_player = 'x'
     else:
         algorithm_player = 'o'     
         
-    return user_player, algorithm_player
+    return user_choice, algorithm_player
 
 def get_user_move(board):
 
     move = 0
-    while (move < 0 or move > 8) or not move:
-        print("Please pick a field from 0 to 8")
+    while (move < 1 or move > 9) or not move:
+        print("Please pick a field from 1 to 9")
         move = uin.integer_input()
 
-        if (move < 0 or move > 8) or not move:
-            print("You have to pick a field between 0 and 8")
+        if (move < 1 or move > 9) or not move:
+            print("You have to pick a field between 1 and 9")
             sleep(1)
             clear_console()
-        if board.flat[move] != ' ':
+        if board.flat[move-1] != ' ':
             move = 0
             print("Field is already occupied. Try another one.")
             print_board(board)
@@ -82,38 +99,35 @@ def print_board(board):
     print("\n")    
 
 def update_board(move, player, board):
-    board.flat[move - 1] = player
+    board.flat[move-1] = player
     return board
 
 def init_game():
     board = np.ndarray(shape=(3, 3), dtype="<U1")
     board[:] = ' '
-    (user_player, algorithm_player) = get_players()
-    print(f"PLAYERS ARE: user: {user_player}, algorithm: {algorithm_player}")
+    (user_choice, algorithm_player) = get_players()
+    print(f"PLAYERS ARE: user: {user_choice}, algorithm: {algorithm_player}")
     print_board(board)
-    return board, user_player, algorithm_player 
+    return board, user_choice, algorithm_player 
 
 def play_tic_tac_toe():
     
     #create board, pick x's and o's for players
-    board, user_player, algorithm_player = init_game()
+    board, user_choice, algorithm_player = init_game()
 
-    #TODO: add algorithm moves and game loop
+    is_maximizing = True if algorithm_player == 'x' else False 
 
+    is_ai_turn = not is_user_first
+    
     # loop for moves (while no victory from either user or algorithm)
-    board = update_board(get_user_move(board), user_player, board)
-    print_board(board)
-
-    ai_turn_indicator = True
-
     while (minimax.heuristic_function(board) is None):
-        if ai_turn_indicator:
-            board = update_board(minimax.minimax(board, np.NINF, np.inf, True)[1], algorithm_player, board)
+        if is_ai_turn:
+            board = update_board(minimax.minimax(board, np.NINF, np.inf, is_maximizing)[1]+1, algorithm_player, board)
         else:
-            board = update_board(get_user_move(board), user_player, board)
+            board = update_board(get_user_move(board), user_choice, board)
             
         print_board(board)
-        ai_turn_indicator = not ai_turn_indicator
+        is_ai_turn = not is_ai_turn
 
 
 play_tic_tac_toe()
